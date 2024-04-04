042930072632febac7bf0ed2c7f878adeec10562
Filipe Andrade <filipeandrade.work@gmail.com>
2024-04-04 12:41:37 -0300

    importei o public.sales

---
+++ main.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+
+def main():
+    spark = SparkSession.builder.appName("lab_dados") \
+        .config('spark.jars.packages', 'org.postgresql:postgresql:42.7.3') \
+            .getOrCreate()
+    
+    public_sales = spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.sales") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+        
+    public_sales.printSchema()
+    
+if __name__ == "__main__":
+    main()
