3d18bd6b95663877582bd9db35c66a98394e287b
Filipe Andrade <filipeandrade.work@gmail.com>
2024-04-04 13:09:13 -0300

    conectei todos as tabelas

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from pyspark.sql import SparkSession
 
-def main():
-    spark = SparkSession.builder.appName("lab_dados") \
-        .config('spark.jars.packages', 'org.postgresql:postgresql:42.7.3') \
-        .getOrCreate()
-    
-    public_sales = spark.read.format("jdbc") \
+def getPublicCategories(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.categories") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+
+def getPublicCustomers(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.customers") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+
+def getPublicProducts(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.products") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+        
+def getPublicSales(spark):
+    return spark.read.format("jdbc") \
         .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
         .option("driver", "org.postgresql.Driver") \
         .option("dbtable", "public.sales") \
@@ -13,7 +36,54 @@ def main():
         .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
         .load()
         
+def getPublicSalesItems(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.sales_items") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+        
+def getPublicSellers(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.sellers") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+        
+def getPublicSuppliers(spark):
+    return spark.read.format("jdbc") \
+        .option("url", "jdbc:postgresql://dpg-co5jfb4f7o1s73a319ag-a.oregon-postgres.render.com:5432/fatorv") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", "public.suppliers") \
+        .option("user", "root") \
+        .option("password", "vW36eDzFKnl2h2ZFCWo7eqgVth9gMC4x") \
+        .load()
+
+def main():
+    spark = SparkSession.builder.appName("lab_dados") \
+        .config('spark.jars.packages', 'org.postgresql:postgresql:42.7.3') \
+        .getOrCreate()
+    
+    public_categories = getPublicCategories(spark)
+    public_customers = getPublicCustomers(spark)
+    public_products = getPublicProducts(spark)
+    public_sales = getPublicSales(spark)
+    public_sales_items = getPublicSalesItems(spark)
+    public_sellers = getPublicSellers(spark)
+    public_suppliers = getPublicSuppliers(spark)
+        
+    
+    public_categories.printSchema()
+    public_customers.printSchema()
+    public_products.printSchema()
     public_sales.printSchema()
+    public_sales_items.printSchema()
+    public_sellers.printSchema()
+    public_suppliers.printSchema()
     
 if __name__ == "__main__":
     main()
